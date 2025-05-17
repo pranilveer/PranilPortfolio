463f7efa66a138f0eb7dbef269e33239d2872eca
Pranil Dilip Veer <veerpranil@gmail.com>
2025-05-17 23:23:49 +0530

    Skills Section Added

--- src/components/skills-section.tsx
+++ src/components/skills-section.tsx
@@ -1,10 +1,20 @@
+import { Box, Typography, Button } from '@mui/material';
 
-const SkillsSection = () => {
-  return (
-    <div>
-      Skils
-    </div>
-  )
-}
+const skills: string[] = ['React', 'TypeScript', 'MUI', 'JavaScript', 'HTML', 'CSS', 'Node.js'];
 
-export default SkillsSection
+const SkillsSection: React.FC = () => (
+  <Box sx={{ mb: 6 }}>
+    <Typography variant="h4" gutterBottom>
+      Skills
+    </Typography>
+    <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1 }}>
+      {skills.map((skill, index) => (
+        <Button variant="outlined" key={index}>
+          {skill}
+        </Button>
+      ))}
+    </Box>
+  </Box>
+);
+
+export default SkillsSection;
